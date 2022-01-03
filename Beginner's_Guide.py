188b9b70b5bf81fa6bad2e5f8bb7a7773eb1c80a
Dambbu <vanjoonseo@gmail.com>
2022-01-02 20:44:51 -0800

    Tried to make Guide=NOT WORKING
    having trouble importing the Beginner's_Guide.md in the code
    don't know what to do in terms of importing it so that the script shows up on the screen.

--- Beginner's_Guide.py
+++ Beginner's_Guide.py
@@ -1,11 +1,17 @@
 import pygame
-#Open up script. 
-
+import sys
+import Beginner's_Guide md.
+pygame.init()
+myfont = pygame.font.SysFont("Times New Roman, 14")
 keys=pygame.key.get_pressed()
-if keys[pygame.K_K]:   
+while True:
+    for event in pygame.event.get():
+        if keys[pygame.K_K]:
+            if keys[pygame.K_K] and keypressed:
+                keypressed = label = myfont.render("Beginner'sGuide.md", 1, (255,255,0))
+        screen.blit(label, (100, 100))
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+ 
 
-
-
-
-
-    if keys[pygame.K_K] and keypressed:
