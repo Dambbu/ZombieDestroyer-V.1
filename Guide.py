bc09eddee3a4a040cfcaa82ee5ab1870813bb8eb
Dambbu <vanjoonseo@gmail.com>
2022-01-02 22:14:44 -0800

    Tried Code for Guide

--- Guide.py
+++ Guide.py
@@ -1,14 +1,15 @@
 import pygame
 import sys
-import Beginner's_Guide md.
-pygame.init()
-myfont = pygame.font.SysFont("Times New Roman, 14")
+pygame.font.init()
+myfont = pygame.font.SysFont('Phosphate', 30)
+
+
 keys=pygame.key.get_pressed()
 while True:
     for event in pygame.event.get():
         if keys[pygame.K_K]:
             if keys[pygame.K_K] and keypressed:
-                keypressed = label = myfont.render("Beginner'sGuide.md", 1, (255,255,0))
+                keypressed = label = myfont.render("Guide.md", 1, (255,255,0))
         screen.blit(label, (100, 100))
         if event.type == pygame.QUIT:
             pygame.quit()
