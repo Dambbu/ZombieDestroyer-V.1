cb94a9154b2da70a316265b55b29e80ea562ff1f
Dambbu <vanjoonseo@gmail.com>
2022-01-08 16:11:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import Bullet
 import Gun
 import Armour
 import Heal
-
+import Guide
 from pygame.constants import KEYDOWN
 #Game Control until we have the code to control bullet damage and reload/speed per gun and Gif. 
 screen_width = 1200
